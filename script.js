6c407e4ec8101ffcae3987a6faec577d1e4c5085
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-02-25 12:46:22 +0100

    html invalid method fixed

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var usernameInput = document.querySelector('.username');
 var textoUsuario = document.querySelector('#textoUsuario');
 usernameInput.addEventListener('keyup',function (event) {
     console.log(event.target.value)
-    textoUsuario.html(event.target.value)
+    textoUsuario.innerHTML=event.target.value;
 })
 // usernameInput.onkeyup =function (event) {
 //     console.log(event.target.value)
