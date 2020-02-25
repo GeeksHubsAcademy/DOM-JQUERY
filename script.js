6bc2dbba88a1e16019311812aa636581055dc268
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-02-25 13:09:22 +0100

    added also password field

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 // var usernameInput=$('.username');
-var usernameInput = document.querySelector('.username');
 // var textoUsuario=$('#textoUsuario');
 // usernameInput.keyup(function (event) {
-//     console.log(event.target.value)
 //     textoUsuario.html(event.target.value)
 // })
-var textoUsuario = document.querySelector('#textoUsuario');
-usernameInput.addEventListener('keyup',function (event) {
-    console.log(event.target.value)
-    textoUsuario.innerHTML=event.target.value;
-})
 // usernameInput.onkeyup =function (event) {
-//     console.log(event.target.value)
 //     textoUsuario.html(event.target.value)
 // }
+var usuario={
+    username:'',
+    password:''
+}
+var usernameInput = document.querySelector('.username');
+var textoUsuario = document.querySelector('#textoUsuario');
+usernameInput.addEventListener('keyup', function (event) {
+    textoUsuario.innerHTML ='<p>'+event.target.value+'</p>';
+})
+
+var passwordInput = document.querySelector('.password');
+var textoPassword = document.querySelector('#textoPass');
+passwordInput.addEventListener('keyup', function (event) {
+    textoPassword.innerText =event.target.value;
+})
